83a97fc3a90d5e82bebfeac47d8ba0fd9f5a1cdf
João Teixeira <joao.teixeira@ifro.edu.br>
2023-03-20 18:33:19 -0400

    validação de entradas

--- aula_01_13-03/src/router.ts
+++ aula_01_13-03/src/router.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.send({
         api_name: 'api-contatos',
         descricao: 'API para gestão de contatos',
@@ -10,7 +10,7 @@ router.get('/', function(req, res) {
     });
 });
 
-router.get('/sobre', function(req, res) {
+router.get('/sobre', function (req, res) {
     res.send({
         name: 'João Teixeira',
         email: 'joao.teixeira@ifro.edu.br',
@@ -18,9 +18,9 @@ router.get('/sobre', function(req, res) {
     });
 });
 
-router.post('/contato', function(req, res) {
-    let ok = false
-    let mensagem = "A propriedade [nome] está indefinida!";
+router.post('/contato', function (req, res) {
+    let ok = true;
+    let mensagem = "Contato salvo com sucesso!";
 
     /*
     if (req.body.nome == undefined) {
@@ -32,20 +32,66 @@ router.post('/contato', function(req, res) {
         ok = true;
     }
     */
-    if (req.body.nome == undefined || req.body.nome == "") {
-       if(req.body.nome == "") {
-        mensagem = "A propriedade [nome] não deve estar em branco!";
-       }
+    // if (req.body.nome == undefined || req.body.nome == "") {
+    //    if(req.body.nome == "") {
+    //     mensagem = "A propriedade [nome] não deve estar em branco!";
+    //    }
 
-    } else {
-        mensagem = "O contato [" + req.body.nome + "] foi salvo com sucesso!";
-        ok = true;
-    }   
+    // } else {
+    //     mensagem = "O contato [" + req.body.nome + "] foi salvo com sucesso!";
+    //     ok = true;
+    // } 
+
+    const inputs = [
+        {
+            name: "nome",
+            message: "A propriedade [nome] não deve estar em indefinida/vazio!"
+        },
+        {
+            name: "email",
+            message: "A propriedade [email] não deve estar em indefinida/vazio!"
+        }
+    ];
+
+    const checkValidate = isValidateObjectRequest(req, inputs);
+
+    console.log(checkValidate);
+
+    if (Array.isArray(checkValidate)) {
+        ok = false;
+
+        mensagem = checkValidate.join(', ');
+    }
 
     res.send({
         success: ok,
         message: mensagem
     })
 });
+
+function isValidateRequest(req: any, inputs: string[]) {
+    if (inputs.length == 0)
+        return false;
+
+    for (let i = 0; i < inputs.length; i++) {
+
+        if (req.body[inputs[i]] == undefined || req.body[inputs[i]] == "")
+            return false;
+    }
+
+    return true;
+}
+
+function isValidateObjectRequest(req: any, inputs: any[]) {
+    let message = [];
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (req.body[inputs[i].name] == undefined || req.body[inputs[i].name] == "") {
+            message.push(inputs[i].message);
+        }
+    }
+
+    return message.length == 0 ? true : message;
+}
 
 export default router;
